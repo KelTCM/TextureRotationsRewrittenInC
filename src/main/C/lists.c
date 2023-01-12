cce36401041042a70300d2f3855b2b97dcb15d5c
KelTRM <52837791+KelTRM@users.noreply.github.com>
2023-01-12 01:35:08 -0600

    Some work on implementing lists

--- src/main/C/lists.c
+++ src/main/C/lists.c
@@ -1,31 +1,47 @@
-// As per the use of lists in TextureFinder, along with the lack of a list type in C, this here is nessesary in order to give a true port
+#include<stdio.h>
+#include<malloc.h>
+#include<stdbool.h>
+#include<string.h>
 
-#include<stdlib.h> // Import stdlib.h for malloc and realloc
-#include<string.h> // And string.h for memcpy
+// Definition for a linked list
+typedef struct LinkedListElement {
+    struct LinkedListElement *Next;
+    size_t ItemSize;
+    void *Item;
+} LinkedListElement;
 
-// Define a macro to create a list
-#define NEW_LIST(type) createList(sizeof(type))
-
-typedef struct List {
-    void *contents;
+// Definition for the linked list base
+typedef struct LinkedListBase {
+    LinkedListElement *First;
     size_t length;
-    size_t size_per_item;
-    int (*add)(List_Ptr list, void* item, size_t sizePerItem);
-} List, *List_Ptr;
+} LinkedList;
 
-// A function to create a list
-List createList(size_t lengthPerItem) {
-    List newList;
-    newList.length = 1
-    newList.contents = malloc(lengthPerItem * newList.length);
-    newList.size_per_item = lengthPerItem;
-
-    newList.add = &add;
+bool CreateList(LinkedList *List) {
+    *list = { NULL, 0 };
+    return true;
 }
 
-// And one to add to a list
-int add(List_Ptr list, void *item) {
-    list->length++; // Add to the length of the list
-    list->contents = realloc(list->length*list->size_per_item); // Allocate the memory required to put the item into the list
-    memcpy(list->contents + ((list->length-1) * list->size_per_item), item, list->size_per_item); // Copy the items memory and put it into the list
+bool AppendList(LinkedList *List, void *Item, size_t ItemSize) {
+    LinkedListElement *Item = List->First;
+    while (true) {
+        if (Item == NULL || Item->length != NULL) Item = Item->length;
+        else break;
+    }
+    Item->Next = malloc(sizeof(LinkedListElement));
+    List->length++;
+}
+
+stuct { bool Found; void *Item } GetItemAt(size_t Item) {
+    LinkedListElement *Item = List.First;
+    size_t CurrentItem = 0;
+    while (true) {
+        if (Item == NULL || Item->length != NULL) Item = Item->length;
+        else { Item = NULL; break };
+
+        if (CurrentItem++ == Item)
+            break;
+    }
+    if (Item == NULL) return { false; NULL };
+
+    return { true; Item->Item };
 }
